d76b25b28361f52a5a69916dfb548ec7bb80456d
ophum <inagaki0106@gmail.com>
2021-05-17 14:33:24 +0000

    change entity

--- pkg/entities/task_entity.go
+++ pkg/entities/task_entity.go
@@ -4,16 +4,17 @@ import "time"
 
 // +gen-ts-entity
 type TaskEntity struct {
-	ID        string       `json:"_id,omitempty"`
-	Title     string       `json:"title"`
-	Plan      int          `json:"plan"`
-	Todos     []TodoEntity `json:"todos"`
-	Assignees []UserEntity `json:"assignees"`
-	ProjectId string       `json:"project_id"`
+	ID          string       `json:"_id,omitempty"`
+	Title       string       `json:"title"`
+	Plan        int          `json:"plan"`
+	Todos       []TodoEntity `json:"todos"`
+	AssigneeIds []string     `json:"assignees"`
+	ProjectId   string       `json:"project_id"`
 }
 
 // +gen-ts-entity
 type TodoEntity struct {
+	AssigneeId    string    `json:"assignee_id,omitempty"`
 	StartDatetime time.Time `json:"start_datetime" ts-type:"string"`
 	EndDatetime   time.Time `json:"end_datetime" ts-type:"string"`
 	Description   string    `json:"description"`
